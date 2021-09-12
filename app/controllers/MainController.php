96844af9cb6c947b3837a66450e653ba4750f547
SAKotov <mozz.kotov@gmail.com>
2021-09-12 18:07:31 +0300

    Crate class Cache, finishing writing the framework.

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -3,6 +3,8 @@
 
 namespace app\controllers;
 
+use ishop\Cache;
+
 class MainController extends AppController
 {
 
@@ -12,7 +14,16 @@ class MainController extends AppController
         $this->setMeta('Главная страница', 'Описание...', 'Ключи');
         $name = 'John';
         $age = 30;
-        $names = ['Andrey', 'Jo'];
+        $names = ['Andrey', 'Jo', 'Mike'];
+        $cache = Cache::instance();
+        //$cache->set('test', $names);
+        $cache->delete('test');
+        $data = $cache->get('test');
+        if (!$data)
+        {
+            $cache->set('test', $names);
+        }
+        debug($data);
         $this->set(compact('name', 'age', 'names', 'posts'));
     }
 }
