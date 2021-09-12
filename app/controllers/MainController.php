39805e2f3b891009ac389d6cd999fe725a0bf3d0
SAKotov <mozz.kotov@gmail.com>
2021-09-12 19:00:20 +0300

    transfer of template files

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,20 +10,6 @@ class MainController extends AppController
 
     public function indexAction()
     {
-        $posts = \R::FindAll('test');
         $this->setMeta('Главная страница', 'Описание...', 'Ключи');
-        $name = 'John';
-        $age = 30;
-        $names = ['Andrey', 'Jo', 'Mike'];
-        $cache = Cache::instance();
-        //$cache->set('test', $names);
-        $cache->delete('test');
-        $data = $cache->get('test');
-        if (!$data)
-        {
-            $cache->set('test', $names);
-        }
-        debug($data);
-        $this->set(compact('name', 'age', 'names', 'posts'));
     }
 }
