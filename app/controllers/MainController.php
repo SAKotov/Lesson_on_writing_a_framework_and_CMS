c881909c5ce2f8d72221d18d4b55d183262435ae
SAKotov <mozz.kotov@gmail.com>
2021-09-12 11:26:10 +0300

    View controller settings

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,8 +7,14 @@ use ishop\App;
 
 class MainController extends AppController
 {
+
     public function indexAction()
     {
-        echo __METHOD__;
+//        echo __METHOD__;
+        $this->setMeta('Главная страница', 'Описание...', 'Ключи');
+        $name = 'John';
+        $age = 30;
+        $names = ['Andrey', 'Jo'];
+        $this->set(compact('name', 'age', 'names'));
     }
 }
--- vendor/ishop/core/base/Controller.php
+++ vendor/ishop/core/base/Controller.php
@@ -12,8 +12,9 @@ abstract class Controller
     public $model;
     public $view;
     public $prefix;
+    public $layout;
     public $data = [];
-    public $meta = [];
+    public $meta = ['title' => '', 'desc' => '', 'keywords' =>''];
 
     public function __construct($route)
     {
@@ -22,6 +23,12 @@ abstract class Controller
         $this->model = $route['controller'];
         $this->view = $route['action'];
         $this->prefix = $route['prefix'];
+    }
+
+    public function getView()
+    {
+        $viewObject = new View($this->route, $this->layout, $this->view, $this->meta);
+        $viewObject->render($this->data);
     }
 
     public function set($data)
